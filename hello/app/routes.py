fdf513d5d272e0155f890d9344b4f4b460fd2974
ZhiquanW <zhiquan.wzq@gmail.com>
2019-12-13 13:07:22 -0500

    tmp update

--- hello/app/routes.py
+++ hello/app/routes.py
@@ -1,5 +1,6 @@
-from app import app
 from flask import render_template
+from app import app
+from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
@@ -14,3 +15,9 @@ def index():
     }
     ]
     return render_template('index.html', user=user,postss=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Sign in',form = form)
+    # return render_template('base.html')
